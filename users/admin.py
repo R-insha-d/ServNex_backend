ddef37fcd0b7af9b565bd38f11496d6e44e00a94
rinshad <rinshadkdn@gmail.com>
2026-03-27 02:07:00 +0530

    for anugrah to pull

--- users/admin.py
+++ users/admin.py
@@ -8,11 +8,15 @@ class CustomUserAdmin(admin.ModelAdmin):
     search_fields = ('email', 'first_name', 'last_name', 'phone')
     ordering = ('email',)
 
+
+
 @admin.register(PasswordResetOTP)
 class PasswordResetOTPAdmin(admin.ModelAdmin):
     list_display = ('user', 'otp', 'created_at', 'is_verified')
     list_filter = ('is_verified', 'created_at')
     search_fields = ('user__email', 'otp')
+    
+
 
 @admin.register(PendingUser)
 class PendingUserAdmin(admin.ModelAdmin):
